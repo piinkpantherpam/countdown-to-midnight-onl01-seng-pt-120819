5bd1324254ce837bbbdfd989aaa4f749b695be52
Pam Cairo <pcairo_90@outlook.com>
2019-12-18 01:34:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
-
-def countdown
+def countdown (integer)
+  while integer > 0 do
+  end
+  puts "#{integer} SECOND(S)!"
+  integer -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (speed)
+  while speed > 0 do
+    puts "#{speed} SECOND (S)!"
+    speed -= 1 
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+  end
