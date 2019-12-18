f6fe3a851e0d9a2d3aea2fc6c852368fc4414aa8
Pam Cairo <pcairo_90@outlook.com>
2019-12-18 01:42:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 def countdown (integer)
   
   while integer > 0 do
-  puts "#{integer} SECOND(S)!"
-  integer -= 1
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
   end
   return "HAPPY NEW YEAR!"
 end
